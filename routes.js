903bdfd2b7ba6ec416246862e513c42ec47e2c48
IsaacMacielH <98364441+IsaacMacielH@users.noreply.github.com>
2023-02-22 17:56:52 -0600

    Add files via upload
    Se agregan los archivos del proyecto

---
+++ routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err);
+        conn.query('SELECT * FROM books', (err, rows)=>{
+            if(err) return res.send(err);
+            res.json(rows);
+        })
+    })
+})
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err);
+        //console.log(req.body);
+        conn.query('INSERT INTO books SET ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err);
+            res.send('book registered!');
+        })
+    })
+})
+
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err);
+        //console.log(req.body);
+        conn.query('DELETE FROM books WHERE id =  ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err);
+            res.send('book deleted!');
+        })
+    })
+});
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err);
+        //console.log(req.body);
+        conn.query('UPDATE books SET ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err);
+            res.send('book updated!');
+        })
+    })
+});
+
+module.exports = routes;
